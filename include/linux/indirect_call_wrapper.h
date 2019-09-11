2913f5e128b18aa75d1b68b3f6b35ecc7035147b
Petr Vorel <pvorel@suse.cz>
2019-09-11 10:36:25 +0200

    DEBUG indirect_call_wrapper.h (broke build)
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- include/linux/indirect_call_wrapper.h
+++ include/linux/indirect_call_wrapper.h
@@ -3,6 +3,7 @@
 #define _LINUX_INDIRECT_CALL_WRAPPER_H
 
 #ifdef CONFIG_RETPOLINE
+#warning #ifdef CONFIG_RETPOLINE
 
 /*
  * INDIRECT_CALL_$NR - wrapper for indirect calls with $NR known builtin
@@ -28,6 +29,7 @@
 #define INDIRECT_CALLABLE_SCOPE
 
 #else
+#warning #else CONFIG_RETPOLINE
 #define INDIRECT_CALL_1(f, f1, ...) f(__VA_ARGS__)
 #define INDIRECT_CALL_2(f, f2, f1, ...) f(__VA_ARGS__)
 #define INDIRECT_CALLABLE_DECLARE(f)
@@ -40,12 +42,16 @@
  * alternatives
  */
 #if IS_BUILTIN(CONFIG_IPV6)
+#warning #if IS_BUILTIN CONFIG_IPV6
 #define INDIRECT_CALL_INET(f, f2, f1, ...) \
 	INDIRECT_CALL_2(f, f2, f1, __VA_ARGS__)
 #elif IS_ENABLED(CONFIG_INET)
+#warning #elif IS_ENABLED(CONFIG_INET)
 #define INDIRECT_CALL_INET(f, f2, f1, ...) INDIRECT_CALL_1(f, f1, __VA_ARGS__)
 #else
+#warning #else
 #define INDIRECT_CALL_INET(f, f2, f1, ...) f(__VA_ARGS__)
 #endif
 
+#error EXIT DUE DEBUG
 #endif
