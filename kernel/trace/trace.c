8eed58c31a39b20a64207f58947bd4e246f16c8f
Christoph Hellwig <hch@lst.de>
2020-05-20 17:58:15 +0200

    fs: make the pipe_buf_operations ->confirm operation optional
    commit b8d9e7f2411b0744df2ec33e80d7698180fef21a upstream.
    
    Just return 0 for success if it is not present.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -7501,7 +7501,6 @@ static bool buffer_pipe_buf_get(struct pipe_inode_info *pipe,
 
 /* Pipe buffer operations for a buffer. */
 static const struct pipe_buf_operations buffer_pipe_buf_ops = {
-	.confirm		= generic_pipe_buf_confirm,
 	.release		= buffer_pipe_buf_release,
 	.get			= buffer_pipe_buf_get,
 };
