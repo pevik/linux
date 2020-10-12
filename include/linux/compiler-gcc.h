d19ee351dab11545928bcf1d948af34393e61846
Petr Vorel <petr.vorel@gmail.com>
2020-10-12 22:48:32 +0200

    Fix compilation for gcc4
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -103,7 +103,8 @@
 #define __gcc_header(x) #x
 #define _gcc_header(x) __gcc_header(linux/compiler-gcc##x.h)
 #define gcc_header(x) _gcc_header(x)
-#include gcc_header(__GNUC__)
+//#include gcc_header(__GNUC__)
+#include "linux/compiler-gcc4.h"
 
 #if !defined(__noclone)
 #define __noclone	/* not needed */
